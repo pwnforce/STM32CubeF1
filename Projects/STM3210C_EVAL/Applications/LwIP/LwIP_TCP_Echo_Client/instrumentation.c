33d1b5e7a0efd2f244c02671ff19183f6bfc8f40
Majid Salehi <64958231+pwnforce@users.noreply.github.com>
2022-01-22 17:20:43 +0100

    Update instrumentation.c

--- Projects/STM3210C_EVAL/Applications/LwIP/LwIP_TCP_Echo_Client/instrumentation.c
+++ Projects/STM3210C_EVAL/Applications/LwIP/LwIP_TCP_Echo_Client/instrumentation.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <string.h>
-// #include <stdio.h>
+#include <stdio.h>
 
 // magic address of one page size (4k) mapped r+w to share data between
 // loader and fimrware constructors
